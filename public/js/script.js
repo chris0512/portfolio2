d68793e9dfca0ae276ac8a5308905c99d6c97e22
Yang Chi <kyang3200@gmail.com>
2023-12-27 18:22:08 +0800

    make the avatar and profile move simultaneously

--- public/js/script.js
+++ public/js/script.js
@@ -4,13 +4,16 @@ function toggleMenu() {
 
 
     const picContainer = document.querySelector(".section__pic-container");
+    const textContainer = document.querySelector(".section__text");
 
     if (picContainer.style.transform === "" || picContainer.style.transform === "translateY(0px)") {
-        // If menu is closed, open it and move avatar down
-        picContainer.style.transform = "translateY(100px)"; // Change this value as needed
+        // If menu is closed, open it and move avatar and text down
+        picContainer.style.transform = "translateY(100px)";  // Change this value as needed
+        textContainer.style.transform = "translateY(100px)";  // Change this value as needed
     } else {
-        // If menu is open, close it and move avatar back to original position
+        // If menu is open, close it and move avatar and text back to original position
         picContainer.style.transform = "translateY(0px)";
+        textContainer.style.transform = "translateY(0px)";
     }
 
     // transition between open and close state from 'open' class
