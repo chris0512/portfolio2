ef46f8329dbedc1cc42cd27aea69fa6126591d88
Yang Chi <kyang3200@gmail.com>
2023-12-27 18:18:37 +0800

    fix the overlay between toggle menu links and avatar

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,17 @@ function toggleMenu() {
     const menuLinks = document.querySelector('.menu-links');
     const hamburgerIcons = document.querySelector('.hamburger-icon');
 
+
+    const picContainer = document.querySelector(".section__pic-container");
+
+    if (picContainer.style.transform === "" || picContainer.style.transform === "translateY(0px)") {
+        // If menu is closed, open it and move avatar down
+        picContainer.style.transform = "translateY(100px)"; // Change this value as needed
+    } else {
+        // If menu is open, close it and move avatar back to original position
+        picContainer.style.transform = "translateY(0px)";
+    }
+
     // transition between open and close state from 'open' class
     menuLinks.classList.toggle('open');
     hamburgerIcons.classList.toggle('open');
